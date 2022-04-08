f6b5f27cbb61eb97598514226bbe4b7d7233fdfc
GigioR <gioprib@gmail.com>
2022-04-08 00:10:00 -0300

    first page basics

---
+++ bees/src/HomePage/HomePage.styles.ts
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const LandingContentWrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+    background: #F2EC54;
+`;
+    
+export const WelcomeContent = styled.div`
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    flex-direction: column;
+    padding: 8px 16px;
+`;
+
+export const FullNameMessage = styled.span`
+    padding: 8px 16px;
+`; 
+
+export const AlphabeticalMessage = styled.span`
+    padding: 8px 16px;
+`;
+
+export const InputAndButtonContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const InputNameContainer = styled.input`
+    width: 300px;
+    height: 43px;
+    border-radius: 4px;
+    padding: 0px 16px;
+    margin: 16px;
+`;
+
+export const InputCheckboxText = styled.label`
+    margin-bottom: 16px;
+`;
+
+export const InputCheckbox = styled.input`
+    margin-right: 8px;
+`;
+
+export const EnterButton = styled.button`
+    border-radius: 4px;
+    background-color: ${props => props.disabled ? "#52525B" : "blue"};
+    color: #FFFFFF;
+    width: 75px;
+    height: 40px;
+    padding: 8px, 16px, 8px, 16px;
+`;
+
+
+export const BeeIcon = styled.img`
+    position: absolute;
+    width: 278px;
+    height: 278px;
+`;
+
+
---
+++ bees/src/HomePage/HomePage.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react"
+import { AlphabeticalMessage, BeeIcon, EnterButton, FullNameMessage, InputAndButtonContainer, InputCheckbox, InputCheckboxText, InputNameContainer, LandingContentWrapper, WelcomeContent } from "./HomePage.styles"
+export const HomePage = () => {
+    const [disabledButton, setDisabledButton] = useState<boolean>(true);
+    const [name, setName] = useState<string>();
+    const [checkboxState, setCheckboxState] = useState<boolean>(false);
+
+    useEffect(() => {
+        if(name && checkboxState) setDisabledButton(false)
+    }, [name, checkboxState])
+
+    const handleSetName = (value: string) => {
+        if(!value.length) setDisabledButton(true);
+        setName(value);
+    }
+
+    const handleCheckboxState = () => {
+        setCheckboxState(!checkboxState)
+        setDisabledButton(true);
+    }
+
+    return (
+        <LandingContentWrapper>
+            <WelcomeContent>
+                <FullNameMessage>Please, enter your full name below</FullNameMessage>
+                <AlphabeticalMessage>Only alphabetical characters are accepted</AlphabeticalMessage>
+                <InputAndButtonContainer>
+                    <InputNameContainer placeholder="Full name" onChange={(e) => handleSetName(e.target.value)}></InputNameContainer>
+                    <InputCheckboxText>
+                        <InputCheckbox type="checkbox" onChange={() => handleCheckboxState()}/>
+                        Are you older than 18 years old?
+                    </InputCheckboxText>
+                    <EnterButton disabled={disabledButton}>Enter</EnterButton>
+                </InputAndButtonContainer>
+            </WelcomeContent>
+            <BeeIcon src="../../public/bee.png" />
+        </LandingContentWrapper>
+    )
+}
